90fe056baa1e105974d6ceed5a69ee642435e256
ArunKumar <156224894+arunkr07@users.noreply.github.com>
2024-08-01 12:33:15 +0530

    First Commit

---
+++ Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Test case
+        int[] nums = {3, 2, 2, 3, 4, 5};
+        int val = 3;
+        
+        int newLength = solution.removeElement(nums, val);
+        
+        // Print the modified array and the new length
+        System.out.println("New length of the array: " + newLength);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
---
+++ TwoSum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i;
+        int sum=0;
+        int j=1;
+        for(i=0;i<nums.length;i++){
+            if(j>i && j<nums.length){
+                sum=nums[i]+nums[j];
+                if(sum==target){
+                    return new int[] {i, j};
+                }
+                j++;
+            }
+        }
+        return null;
+    }
+}
+public class TwoSum{
+    public static void main(String[] args) {
+        int[] nums={3,2,3};
+        int target=6;
+        Solution obj=new Solution();
+        obj.twoSum(nums, target);
+    }
+}
